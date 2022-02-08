eb7561a2c4c34c2e55053ed32200667eb6e8de81
Sylvain Lafay <sylvain.lafay@ign.fr>
2022-02-08 14:10:45 +0100

    position avertissement espace (closes #5)

--- assets/js/components/notifications-bar.js
+++ assets/js/components/notifications-bar.js
@@ -2,6 +2,7 @@ $(function () {
     $('.notifications-bar-close-btn').each(function () {
         $(this).on('click', function () {
             $(this).closest('.notifications-bar').remove();
+            $(".body-wrapper").removeClass("with-notifications-bar");
         });
     });
 });
