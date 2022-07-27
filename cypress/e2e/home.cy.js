aa6659e2d022feba335ce6a9d8d785f13f915dda
Orka Arnest CRUZE <orka.cruze@ign.fr>
2022-07-27 15:24:41 +0200

    cypress: add more tests

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,8 +1,27 @@
-describe("la page d'accueil", () => {
+// TODO revoir comment structurer les tests (describe, context, it)
 
-    it('contient comme titre principal et bouton commencer', () => {
+describe("la page d'accueil", () => {
+    it('contient comme titre principal', () => {
         cy.visit('/')
         cy.get('.container-content > h1').contains("Transformez vos données géographiques en tuiles vectorielles simplement et diffusez-les n'importe où")
-        cy.get('.container-content > div > a.btn').contains('Commencer').should('have.attr', 'href').and('include', '/login')
+    })
+
+    context('utilisateur non-connecté', () => {
+        it('contient comme bouton commencer', () => {
+            cy.visit('/')
+            cy.get('.container-content > div > a.btn').as('btn-commencer').contains('Commencer').should('have.attr', 'href').and('include', '/login')
+        })
+    })
+
+    context('utilisateur de test connecté', () => {
+        it('contient comme bouton commencer', () => {
+            cy.fakeLogin()
+
+            cy.visit('/')
+            cy.get('.container-content > div > a.btn').as('btn-commencer').contains('Commencer').should('have.attr', 'href').and('include', '/datastores')
+
+            cy.get('@btn-commencer').click()
+            cy.url().should('include', '/datastores')
+        })
     })
 })
