9b7884d5005b9d2d70115dfee3901655548df960
Orka Arnest CRUZE <orka.cruze@ign.fr>
2022-10-19 16:26:25 +0200

    remplace LogoutListener par LogoutSubscriber

---
+++ src/Listener/LogoutSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Listener;
+
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Stevenmaguire\OAuth2\Client\Provider\Keycloak;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
+
+class LogoutSubscriber implements EventSubscriberInterface
+{
+    private ClientRegistry $clientRegistry;
+    private UrlGeneratorInterface $urlGenerator;
+
+    public function __construct(ClientRegistry $clientRegistry, UrlGeneratorInterface $urlGenerator)
+    {
+        $this->clientRegistry = $clientRegistry;
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [LogoutEvent::class => 'onLogout'];
+    }
+
+    public function onLogout(LogoutEvent $event): void
+    {
+        $session = $event->getRequest()->getSession();
+        $session->remove('keycloak_token');
+
+        /** @var Keycloak */
+        $keycloak = $this->clientRegistry->getClient('keycloak')->getOAuth2Provider();
+
+        $homeUrl = $this->urlGenerator->generate('plage_home', [], UrlGeneratorInterface::ABSOLUTE_URL);
+        $response = new RedirectResponse($keycloak->getLogoutUrl([
+            'redirect_uri' => $homeUrl,
+        ]));
+        $event->setResponse($response);
+    }
+}
